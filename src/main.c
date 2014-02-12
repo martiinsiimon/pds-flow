70aba27c73cf3174c9a4ee8305346bbf458eedc5
Martin Simon <martiin.siimon@gmail.com>
2014-02-13 00:15:11 +0100

    Parsing almost done

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
 #include <string.h>
+#include <dirent.h>
 
 #include "main.h"
 
-void print_flow(struct flow *fl) {
+void print_flow(struct flow *fl)
+{
     char srcip[INET6_ADDRSTRLEN];
     char dstip[INET6_ADDRSTRLEN];
+
     inet_ntop(AF_INET6, &(fl->src_addr), srcip, INET6_ADDRSTRLEN);
     inet_ntop(AF_INET6, &(fl->dst_addr), dstip, INET6_ADDRSTRLEN);
     fprintf(stdout, "%s:%d -> %s:%d, pkts: %"PRIi64" , bytes: %"PRIi64" \n", srcip, ntohs(fl->src_port), dstip, ntohs(fl->dst_port),
@@ -19,7 +22,8 @@ void print_flow(struct flow *fl) {
             __builtin_bswap64(fl->bytes));
 }
 
-void printHelp(char *name) {
+void printHelp(char *name)
+{
     fprintf(stdout, "Usage: %s -f directory -a aggregation -s sort\n", name);
     fprintf(stdout, "       %s -h\n", name);
     fprintf(stdout, "       %s --help\n", name);
@@ -28,28 +32,92 @@ void printHelp(char *name) {
     fprintf(stdout, "    sort         sort key [srcport, dstport, packets, bytes]\n\n");
 }
 
-void printError(char *msg) {
-    fprintf(stderr, "%s\n", msg);
+void printError(char *msg)
+{
+    fprintf(stderr, "ERR: %s\n", msg);
 }
 
-int main(int argc, char *argv[]) {
-    int opt;
-    char *directory;
-    char *sortkey;
-    char *aggregationkey;
+int parseSortKey(char *key)
+{
+    if (strcmp(key, "srcport") == 0)
+        return EN_SORT_SRCPORT;
+    else if (strcmp(key, "dstport") == 0)
+        return EN_SORT_DSTPORT;
+    else if (strcmp(key, "packets") == 0)
+        return EN_SORT_PACKETS;
+    else if (strcmp(key, "bytes") == 0)
+        return EN_SORT_BYTES;
+    else
+        return EN_ERROR;
+}
 
-    if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+int parseAggKey(char *key, int * mask)
+{
+    char * p = strchr(key, '/');
+    if (p != NULL)
+    {
+        //printf("%d", p - key); //FIXME compare with substring, check if position is not behind the end and convert the rest to integer (mask))
+        if (strcmp(key, "srcip/mask") == 0)
+        {
+            *mask = 10; //FIXME
+            return EN_AGG_SRCIP;
+        }
+        else if (strcmp(key, "dstip/mask") == 0)
+        {
+            *mask = 10; //FIXME
+            return EN_AGG_DSTIP;
+        }
+    }
+    else if (strcmp(key, "srcport") == 0)
+        return EN_AGG_SRCPORT;
+    else if (strcmp(key, "dstport") == 0)
+        return EN_AGG_DSTPORT;
+    else
+        return EN_ERROR;
+}
+
+void addRecord(struct flow *fl, int agsign, int mask)
+{
+    //FIXME
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    char *directory;
+    int sortkey;
+    int aggkey;
+    int mask;
+
+    if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
+    {
         /* Help requested */
         printHelp(argv[0]);
         return (EXIT_SUCCESS);
     }
-    else if (argc == 7 && strcmp(argv[1], "-f") == 0 && strcmp(argv[3], "-a") == 0 && strcmp(argv[5], "-s") == 0) {
+    else if (argc == 7 && strcmp(argv[1], "-f") == 0 && strcmp(argv[3], "-a") == 0 && strcmp(argv[5], "-s") == 0)
+    {
         /* Correct parameters */
-        directory = argv[2];
-        sortkey = argv[4];
-        aggregationkey = argv[6];
+        directory = argv[2]; /* Will be checked by openning */
+
+        /* Check aggkey */
+        if ((sortkey = parseAggKey(argv[4], &mask)) == EN_ERROR)
+        {
+            printError("Invalid aggregation key!");
+            printHelp(argv[0]);
+            return (EXIT_FAILURE);
+        }
+
+        /* Check sortkey */
+        if ((sortkey = parseSortKey(argv[6])) == EN_ERROR)
+        {
+            printError("Invalid sort key!");
+            printHelp(argv[0]);
+            return (EXIT_FAILURE);
+        }
     }
-    else {
+    else
+    {
         /* Invalid parameters! */
         printError("Invalid parameters!");
         printHelp(argv[0]);
@@ -57,14 +125,70 @@ int main(int argc, char *argv[]) {
     }
 
 
-    FILE *fp = fopen(directory, "rb");
-    struct flow fl;
-    size_t n = 0;
-    while ((n = fread(&fl, sizeof (struct flow), 1, fp)) != 0) {
-        print_flow(&fl);
-        break;
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(directory)) != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            /* Skip special unix files . and .. */
+            if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+                continue;
+
+            /* Get file name */
+            char * file = malloc(strlen(ent->d_name) + strlen(directory) + 1 + 1);
+            strcpy(file, directory);
+            strcat(file, "/");
+            strcat(file, ent->d_name);
+
+            // DBG //////////////////////////////////////////////
+            //fprintf(stdout, "%s\n", file);
+            // ENDDBG ///////////////////////////////////////////
+
+            /* Start to parse file by chosen aggregation */
+            FILE *fp = fopen(file, "rb");
+            struct flow fl;
+            size_t n = 0;
+            while ((n = fread(&fl, sizeof (struct flow), 1, fp)) != 0)
+            {
+                /*TODO*/
+                //print_flow(&fl);
+                addRecord(&fl, aggkey, mask);
+                //break;
+            }
+
+            /* Close the file */
+            fclose(fp);
+
+            /* Free the file name */
+            free(file);
+        }
+        closedir(dir);
     }
-    fclose(fp);
+    else
+    {
+        /* Unable to open directory */
+        printError("Unable to open given directory!");
+        return (EXIT_FAILURE);
+    }
+
+    /* Sort the internal structure */
+    /* TODO */
+
+    /* Print the sorted internal structure */
+    /* TODO */
+
+
+    /*
+        FILE *fp = fopen(directory, "rb");
+        struct flow fl;
+        size_t n = 0;
+        while ((n = fread(&fl, sizeof (struct flow), 1, fp)) != 0) {
+            print_flow(&fl);
+            break;
+        }
+        fclose(fp);
+     */
 
     return (EXIT_SUCCESS);
 }
--- src/main.h
+++ src/main.h
@@ -17,10 +17,39 @@ struct flow
     uint64_t bytes;
 };
 
+struct data_struct
+{
+    union
+    {
+        uint16_t port;
+        struct in6_addr addr;
+    };
+    uint64_t packets;
+    uint64_t bytes;
+};
 
-// prototypes //
+/* Sort key values */
+#define EN_SORT_SRCPORT 1
+#define EN_SORT_DSTPORT 2
+#define EN_SORT_PACKETS 3
+#define EN_SORT_BYTES 4
+
+/* Aggregation key values */
+#define EN_AGG_SRCIP 1
+#define EN_AGG_DSTIP 2
+#define EN_AGG_SRCPORT 3
+#define EN_AGG_DSTPORT 4
+
+/* Other values */
+#define EN_ERROR -1
+
+/* Prototypes */
 void print_flow(struct flow *fl);
 void printHelp(char *name);
 void printError(char *msg);
+
+int parseSortKey(char *key);
+int parseAggKey(char *key, int * mask);
+void addRecord(struct flow *fl, int agsign, int mask);
 
 #endif /* MAIN_H */
